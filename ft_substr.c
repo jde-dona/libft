968cf49ddcac1137a2fcd9068a17d41c4558ee59
Niccolo Bordoni <nbordoni@c2r3p2.42firenze.it>
2022-10-06 14:29:39 +0200

    substr added

---
+++ ft_substr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nbordoni <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/06 14:17:45 by nbordoni          #+#    #+#             */
+/*   Updated: 2022/10/06 14:29:08 by nbordoni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdlib.h>
+#include <stddef.h>
+char *ft_substr(char const *s, unsigned int start, size_t len)
+{
+	int	i;
+	char	*dest;
+
+	if (len <= 0)
+		return (NULL);
+	start--;
+	i = 0;
+	dest = malloc(len * sizeof(char));
+	if (dest == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		dest[i] = s[start];
+		i++;
+		start++;
+	}
+	return (dest);
+}
