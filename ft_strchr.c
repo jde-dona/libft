19e6136f6d18788945578141c8342a4c5f00588f
Jacopo De Dona <japodode@gmail.com>
2022-10-06 13:42:04 +0200

    ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,26 +1,14 @@
-#include <stdio.h>
-char	*ft_strchr(char *str, int c)
+char    *ft_strchr(const char *str, int c)
 {
-	int i = 0;
-	int a = 0;
-	int d = 0;
-	char *str1;
-	str1 = str;
-	while (str[i] != '\0')
-	{
-		if (d == 0 && str[i] == c)
-		{
-			d = 1;
-			str1[a] = str[i];
-			a++;
-		}
-		else if (d == 1)
-		{
-			str1[a] = str[i];
-			a++;
-		}
-		i++;
-	}
-	str1[a] = '\0';
-	return str1;
-}
+    int    i;
+    i = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] == c)
+        {
+            return ((char *)&str[i]);
+        }
+        i++;
+    }
+    return (NULL);
+}	
