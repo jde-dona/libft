9b1e6b08553522b8f06b8deb81f333ffd7104b82
Niccolò Bordoni <110161074+jblackiex@users.noreply.github.com>
2022-10-06 13:19:42 +0200

    Create ft_strnstr.c

---
+++ ft_strnstr.c
@@ -0,0 +1,25 @@
+char	*ft_strnstr(char *str, char *to_find, unsigned int n)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (to_find[0] == '\0')
+	{
+		return (str);
+	}
+	while (str[i] != '\0' && (i < n))
+	{
+		j = 0;
+		while (str[i + j] != '\0' && str[i + j] == to_find[j])
+		{
+			if (to_find[j + 1] == '\0')
+			{
+				return (&str[i]);
+			}
+			++j;
+		}
+		++i;
+	}
+	return (0);
+}
